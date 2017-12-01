912ffaa006d5bee004d3c69f5016ebec5eaf682a
wvbirder <wvbirder@comcast,net>
2017-11-30 21:33:48 -0500

    Updated main.java

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -95,7 +95,7 @@ public class Main {
         System.out.println();
         System.out.println("Press enter to stop the server...");
         
-        System.in.read();
+    //    System.in.read();
         server.shutdown();
     }
 }
